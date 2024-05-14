df33812459b2e4cbcd317c39e105e32670519426
Zoom <xabou2010@gmail.com>
2024-05-13 21:45:57 -0600

    go go go

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,80 +1,18 @@
-import type { DocsThemeConfig } from 'nextra-theme-docs';
-import { useConfig } from 'nextra-theme-docs';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: (
-    <div className="flex items-center gap-2">
-      <span className="font-bold">ZoomHandker</span>
-    </div>
-  ),
-  chat: {
-    link: '',
-  },
+  logo: <span>My Project</span>,
   project: {
-    link: '',
+    link: 'https://github.com/shuding/nextra-docs-template',
   },
-  docsRepositoryBase:
-    '',
-  useNextSeoProps() {
-    const { asPath } = useRouter();
-    if (asPath !== '/') {
-      return {
-        titleTemplate: '%s – ZoomHandler',
-      };
-    }
+  chat: {
+    link: 'https://discord.com',
   },
-  gitTimestamp({ timestamp }) {
-    const { locale, asPath } = useRouter();
-
-    if (asPath !== '/') {
-      return (
-        <>
-          Last updated on:{' '}
-          <time dateTime={timestamp.toISOString()}>
-            {timestamp.toLocaleDateString(locale, {
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric',
-            })}
-          </time>
-        </>
-      );
-    }
-  },
-  head: (meta: any) => {
-    const { frontMatter, title } = useConfig();
-    const { asPath } = useRouter();
-
-    let ogTitle = `${title} – ZoomHandler`;
-
-    if (asPath === '/') {
-      ogTitle = title;
-    }
-
-    const ogDescription = frontMatter.description || '';
-
-    return (
-      <>
-        <meta content={ogTitle} property="og:title" />
-        <meta content={ogDescription} property="og:description" />
-        <meta content={asPath} property="og:url" />
-        <meta content="/favicon.png" property="og:image" />
-        <meta content="#fab259" data-react-helmet="true" name="theme-color" />
-      </>
-    );
-  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: (
-      <span>
-        MIT {new Date().getFullYear()} ©{' '}
-        <a href="" target="_blank">
-          Zoom - TheOldZoom
-        </a>
-      </span>
-    ),
+    text: 'Nextra Docs Template',
   },
-};
+}
 
-export default config;
+export default config
